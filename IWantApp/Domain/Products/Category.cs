7669de0b2ba02112f8e0b2daa5346aabad63040b
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-08-29 21:37:10 -0300

    Validando campos restantes de Category

--- IWantApp/Domain/Products/Category.cs
+++ IWantApp/Domain/Products/Category.cs
@@ -7,13 +7,19 @@ public class Category : Entity
     public string Name { get; set; }
     public bool Active { get; set; }
 
-    public Category(string name)
+    public Category(string name, string createdBy, string editedBy)
     {
         var contract = new Contract<Category>()
-            .IsNotNull(name, "Name", "Nome é obrigatório");
+            .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório")
+            .IsNotNullOrEmpty(createdBy, "CreatedBy", "CreatedBy é obrigatório")
+            .IsNotNullOrEmpty(editedBy, "EditedBy", "EditedBy é obrigatório");
         AddNotifications(contract);
 
         Name = name;
         Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now; 
     }
 }

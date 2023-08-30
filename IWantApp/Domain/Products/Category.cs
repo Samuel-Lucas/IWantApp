3bff69eb3dde00687be3793a9b13552bd031ea8d
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-08-29 21:28:33 -0300

    Utilizando Flunt

--- IWantApp/Domain/Products/Category.cs
+++ IWantApp/Domain/Products/Category.cs
@@ -1,7 +1,19 @@
+using Flunt.Validations;
+
 namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; } = null!;
-    public bool Active { get; set; } = true;
+    public string Name { get; set; }
+    public bool Active { get; set; }
+
+    public Category(string name)
+    {
+        var contract = new Contract<Category>()
+            .IsNotNull(name, "Name", "Nome é obrigatório");
+        AddNotifications(contract);
+
+        Name = name;
+        Active = true;
+    }
 }

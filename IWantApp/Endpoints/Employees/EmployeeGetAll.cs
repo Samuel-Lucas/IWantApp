1e0021e650b7a8ff0c13d3076ed116cd8c5e6a5b
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-09-24 19:48:35 -0300

    Refatorando paginação com o dapper

--- IWantApp/Endpoints/Employees/EmployeeGetAll.cs
+++ IWantApp/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,6 @@
+using Dapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -7,19 +9,18 @@ public class EmployeeGetAll
     public static string Template => "/employees";
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
+    public const string Query = @"SELECT Email, ClaimValue as Name
+                                    FROM AspNetUsers u INNER JOIN AspNetUserClaims c ON u.Id = c.UserId
+                                    AND ClaimType = 'Name'
+                                    ORDER BY Name
+                                    OFFSET ((@page - 1) * @rows) ROWS FETCH NEXT @rows ROWS ONLY";
 
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int page, int rows, IConfiguration configuration)
     {
-        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
-        var employees = new List<EmployeeResponse>();
-
-        foreach (var user in users)
-        {
-            var claims = userManager.GetClaimsAsync(user).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            employees.Add(new EmployeeResponse(user.Email, userName));
-        }
+        var db = new SqlConnection(configuration["ConnectionStrings:SqlServer"]);
+        var employees = db.Query<EmployeeResponse>(
+            Query,
+            new { page, rows });
 
         return Results.Ok(employees);
     }

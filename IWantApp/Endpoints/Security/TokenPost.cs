ce201805b41da54a92fcc410e7ead7ad5f624279
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-12-10 15:37:30 -0300

    Ajuste na duração da expiração do token em ambiente de desenvolvimento

--- IWantApp/Endpoints/Security/TokenPost.cs
+++ IWantApp/Endpoints/Security/TokenPost.cs
@@ -14,10 +14,12 @@ public class TokenPost
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static IResult Action(LoginRequest loginRequest,
-                                 IConfiguration configuration, 
-                                 UserManager<IdentityUser> userManager,
-                                 ILogger<TokenPost> log)
+    public static IResult Action(
+        LoginRequest loginRequest,
+        IConfiguration configuration, 
+        UserManager<IdentityUser> userManager,
+        ILogger<TokenPost> log,
+        IWebHostEnvironment environment)
     {
         log.LogInformation("Getting token");
         
@@ -45,7 +47,8 @@ public class TokenPost
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
             Audience = configuration["JwtBearerTokenSettings:Audience"],
             Issuer = configuration["JwtBearerTokenSettings:Issuer"],
-            Expires = DateTime.UtcNow.AddSeconds(30)
+            Expires = environment.IsDevelopment() || environment.IsStaging() ? 
+                DateTime.UtcNow.AddYears(1) : DateTime.UtcNow.AddMinutes(2)
         };
 
         var tokenHandler = new JwtSecurityTokenHandler();
